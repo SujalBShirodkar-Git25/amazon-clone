be7e6d1be2a3ec8089b0311bd628a33479b76dd3
SujalBShirodkar <205524315+SujalBShirodkar-Git25@users.noreply.github.com>
2025-06-12 23:18:50 +0530

    Updates from Lesson 15 - External Libraries

--- data/cart.js
+++ data/cart.js
@@ -3,11 +3,13 @@ export let cart = JSON.parse(localStorage.getItem('cart'));
 if(!cart){
   cart = [{
     productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity: 2
+    quantity: 2,
+    deliveryOptionId: '1'
   },
   {
     productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity: 1
+    quantity: 1,
+    deliveryOptionId: '2'
   }];
 }
 
@@ -32,9 +34,11 @@ export function addToCart(productId){
     matchingItem.quantity += quantity;
   }
   else{
+    const deliveryOptionId = '1';
     cart.push({
       productId,
-      quantity
+      quantity,
+      deliveryOptionId
     });
   }
 
@@ -72,5 +76,19 @@ export function updateQuantity(productId,newQuantity){
       cartItem.quantity = newQuantity;
     }
   });
+  saveToStorage();
+}
+
+export function updateDeliveryOption(productId,deliveryOptionId){
+  let matchingItem;
+
+  cart.forEach((cartItem) => {
+    if(productId === cartItem.productId){
+      matchingItem = cartItem;
+    }
+  });
+
+  matchingItem.deliveryOptionId = deliveryOptionId;
+
   saveToStorage();
 }
