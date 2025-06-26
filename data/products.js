65be963734acead2c130729f29db1da55835e5f2
SujalBShirodkar <205524315+SujalBShirodkar-Git25@users.noreply.github.com>
2025-06-27 00:54:48 +0530

    Updates from Lesson 18 - Backend

--- data/products.js
+++ data/products.js
@@ -77,6 +77,52 @@ export class Appliance extends Product{
   }
 }
 
+export let products = [];
+
+export function loadProductsFetch(){
+  const promise = fetch('https://supersimplebackend.dev/products').then((response) => {
+    return response.json();
+  }).then((productsData) => {
+    products = productsData.map((productDetails) => {
+      if(productDetails.type === 'clothing'){
+        return new Clothing(productDetails);
+      }
+      return new Product(productDetails);
+    });
+
+    console.log('load products');
+  }).catch((error) => {
+    console.log('Unexpected error. Please try again later.');
+  });
+
+  return promise;
+}
+
+export function loadProducts(fun){
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load',() => {
+    products = JSON.parse(xhr.response).map((productDetails) => {
+      if(productDetails.type === 'clothing'){
+        return new Clothing(productDetails);
+      }
+      return new Product(productDetails);
+    });
+
+    console.log('load products');
+
+    fun();
+  });
+
+  xhr.addEventListener('error',(error) => {
+    console.log('Unexpected error. Please try again later.');
+  });
+
+  xhr.open('GET','https://supersimplebackend.dev/products');
+  xhr.send();
+}
+
+/*
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -776,4 +822,4 @@ export const products = [
     return new Appliance(productDetails);
   }
   return new Product(productDetails);
-});
+});*/
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,23 @@
 import {renderCheckoutHeader} from './checkout/checkoutHeader.js';
 import {renderOrderSummary} from './checkout/orderSummary.js';
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
+import {loadProductsFetch} from '../data/products.js';
+import {cart} from '../data/cart.js';
 
-renderCheckoutHeader();
-renderOrderSummary();
-renderPaymentSummary();
+async function loadPage(){
+  try{
+    await loadProductsFetch();
+    const value = await new Promise((resolve,reject) => {
+      cart.loadCart(() => {
+        resolve('value3');
+      });
+    });
+  }
+  catch(error){
+    console.log('Unexpected error. Please try again later.');
+  }
+  renderCheckoutHeader();
+  renderOrderSummary();
+  renderPaymentSummary();
+}
+loadPage();
