0cda7163c103c7fdae651eccb117b8cc8bc946e8
SujalBShirodkar <205524315+SujalBShirodkar-Git25@users.noreply.github.com>
2025-06-28 20:08:08 +0530

    Renamed amazon.html to index.html

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -118,13 +118,13 @@ function renderProductsGrid(){
 
   document.querySelector('.js-search-button').addEventListener('click',() => {
     const searchTerm = document.querySelector('.js-search-bar').value;
-    window.location.href = `amazon.html?search=${searchTerm}`;
+    window.location.href = `index.html?search=${searchTerm}`;
   });
 
   document.querySelector('.js-search-bar').addEventListener('keydown', (event) => {
     if (event.key === 'Enter') {
       const searchTerm = document.querySelector('.js-search-bar').value;
-      window.location.href = `amazon.html?search=${searchTerm}`;
+      window.location.href = `index.html?search=${searchTerm}`;
     }
   });
 }
